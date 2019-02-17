621b4c043fcc1c2343ab12fde7b757609f0a6a74
只会番茄炒蛋 <673395239@qq.com>
2019-02-17 08:42:23 +0800

    node介绍

---
+++ src/common.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Author: Tank
+ * @GitHub: https://github.com/zhihuifanqiechaodan
+ * @Date: 2019-02-16 16:39:30
+ * @LastEditTime: 2019-02-16 17:05:58
+ */
+exports.a = 1
+module.exports = {
+    b: 1
+}
